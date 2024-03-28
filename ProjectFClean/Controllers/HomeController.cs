31f93ee4f5af1a10aac71ce5df19ff0543c08185
vietlong231 <117057002+vietlong231@users.noreply.github.com>
2024-03-28 14:29:06 +0700

    Add project files.

---
+++ ProjectFClean/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using ProjectFClean.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace ProjectFClean.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ProjectFCleanEntities3 _db;
+
+        public HomeController()
+        {
+            _db = new ProjectFCleanEntities3();
+        }
+
+        public ActionResult Index()
+        {
+            List<Housekeeper> listHousekeeper = _db.Housekeepers.ToList();
+            return View(listHousekeeper);
+        }
+
+        [HttpGet]
+        public ActionResult Search(string gender, string location, string name, string service)
+        {
+            // Lọc danh sách HouseKeeper dựa trên các tham số tìm kiếm
+            IQueryable<Housekeeper> housekeepers = _db.Housekeepers;
+
+            if (!string.IsNullOrEmpty(gender))
+            {
+                housekeepers = housekeepers.Where(h => h.Gender == gender);
+            }
+
+            if (!string.IsNullOrEmpty(location))
+            {
+                housekeepers = housekeepers.Where(h => h.Distrinct == location);
+            }
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                housekeepers = housekeepers.Where(h => h.Name.Contains(name));
+            }
+
+            if (!string.IsNullOrEmpty(service))
+            {
+                // Kiểm tra xem housekeeper có chứa kỹ năng service trong danh sách kỹ năng của mình hay không
+                housekeepers = housekeepers.Where(h => h.Skill.Contains(service));
+            }
+
+            List<Housekeeper> searchResult = housekeepers.ToList();
+
+            return PartialView("_HousekeeperList", searchResult);
+        }
+
+        public ActionResult Sort(int sortOption)
+        {
+            // Get the list of housekeepers from the database
+            var housekeepers = _db.Housekeepers.ToList();
+
+            // Perform sorting based on sortOption
+            switch (sortOption)
+            {
+                case 1: // Price Ascending
+                    housekeepers = housekeepers.OrderBy(h => h.Price).ToList();
+                    break;
+                case 2: // Price Decreasing
+                    housekeepers = housekeepers.OrderByDescending(h => h.Price).ToList();
+                    break;
+                case 3: // Experience Decreasing
+                    housekeepers = housekeepers.OrderByDescending(h => h.Experiment).ToList();
+                    break;
+                default:
+                    // Default sorting (if necessary)
+                    break;
+            }
+
+            // Return the sorted list as a partial view
+            return PartialView("_HousekeeperList", housekeepers);
+        }
+    }
+}
---
+++ ProjectFClean/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProjectFClean")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectFClean")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dcc6bd41-368b-491a-b92f-3d2a100ca245")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
